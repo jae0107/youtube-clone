3c98fd6fd092724fc704958db9c2937b7b645508
jae0107 <ghjgjh0107@gmail.com>
2021-05-27 23:57:14 +0930

    update homepage css

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -3,14 +3,18 @@ import { Container } from 'react-bootstrap'
 import Header from './components/header/Header'
 import Siderbar from './components/siderbar/Siderbar'
 import HomeScreen from './screens/homeScreen/HomeScreen'
+import './_app.scss'
 
 const App = () => {
+
+    
+
     return (
         <>
             <Header></Header>
-            <div className="app_container">
+            <div className="app__container border border-info">
                 <Siderbar></Siderbar>
-                <Container fliid className="app__main">
+                <Container fliid className="app__main border border-warning">
                     <HomeScreen></HomeScreen>
                 </Container>
             </div>
--- youtube-clone/src/components/siderbar/Siderbar.js
+++ youtube-clone/src/components/siderbar/Siderbar.js
@@ -1,11 +1,45 @@
 import React from 'react'
 import './_sidebar.scss'
+import { MdSubscriptions, MdExitToApp, MdThumbUp, MdHistory, MdLibraryBooks, MdHome, MdSentimentDissatisfied } from 'react-icons/md'
 
 const Siderbar = () => {
     return (
-        <div>
-            Siderbar
-        </div>
+        <nav className="border border-danger sidebar">
+            <li>
+                <MdHome size={23} />
+               <span>Home</span>
+            </li>
+            <li>
+               <MdSubscriptions size={23} />
+               <span>Subscriptions</span>
+            </li>
+            <li>
+                <MdThumbUp size={23} />
+                <span>Liked Video</span>
+            </li>
+            <li>
+                <MdHistory size={23} />
+                <span>History</span>
+            </li>
+
+            <li>
+                <MdLibraryBooks size={23} />
+                <span>Library</span>
+            </li>
+            <li>
+                <MdSentimentDissatisfied size={23} />
+                <span>I don't Know</span>
+            </li>
+
+            <hr/>
+
+            <li>
+                <MdExitToApp size={23} />
+                <span>Log Out</span>
+            </li>
+
+            <hr/>
+        </nav>
     )
 }
 
