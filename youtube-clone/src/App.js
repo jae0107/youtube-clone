4b435f81f90661992eb5305387dc228e0575f48b
jae0107 <ghjgjh0107@gmail.com>
2021-05-28 06:29:44 +0930

    add Router

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,27 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom'
+
 import Header from './components/header/Header'
 import Siderbar from './components/siderbar/Siderbar'
 import HomeScreen from './screens/homeScreen/HomeScreen'
 import LoginScreen from './screens/loginScreen/LoginScreen'
 import './_app.scss'
 
-const App = () => {
+const Layout = ({ children }) => {
     const [sidebar, toggleSidebar] = useState(false);
    
     const handleToggleSidebar = () => toggleSidebar(value => !value);
 
     return (
-        // <>
-        //     <Header handleToggleSidebar={handleToggleSidebar}></Header>
-        //     <div className="app__container border border-info">
-        //         <Siderbar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar}></Siderbar>
-        //         <Container fliid className="app__main border border-warning">
-        //             <HomeScreen></HomeScreen>
-        //         </Container>
-        //     </div>
-        // </>
-        <LoginScreen></LoginScreen>
+        <>
+            <Header handleToggleSidebar={handleToggleSidebar}></Header>
+            <div className="app__container">
+                <Siderbar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar}></Siderbar>
+                <Container fliid className="app__main">
+                    { children }
+                </Container>
+            </div>
+        </>
+    )
+}
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path='/' exact>
+                    <Layout>
+                        <HomeScreen></HomeScreen>
+                    </Layout>
+                </Route>
+                <Route path='/auth'>
+                    <LoginScreen></LoginScreen>
+                </Route>
+
+                <Route path='/search'>
+                    <Layout>
+                        <h1>Search Results</h1>
+                    </Layout>
+                </Route>
+
+                <Route>
+                    <Redirect to='/'></Redirect>
+                </Route>
+            </Switch>
+        </Router>
     )
 }
 
