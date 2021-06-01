477ecfef20b98b011613139cbe85de3f252df308
jae0107 <ghjgjh0107@gmail.com>
2021-06-01 21:45:41 +0930

    add auth

--- youtube-clone/src/screens/loginScreen/LoginScreen.js
+++ youtube-clone/src/screens/loginScreen/LoginScreen.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import './loginScreen.scss'
+import { useDispatch } from 'react-redux'
+import { login } from '../../redux/actions/auth.action'
 
 const LoginScreen = () => {
+    
+    const dispatch = useDispatch();
+
+    const handleLogin = () => {
+        dispatch(login());
+    }
+
     return (
         <div className='login'>
             <div className='login__container'>
             <img src='http://pngimg.com/uploads/youtube/youtube_PNG2.png' alt='' />
-            <button>Login With google</button>
+            <button onClick={handleLogin}>Login With google</button>
             <p>This Project is made using YOUTUBE DATA API</p>
             </div>
         </div>
