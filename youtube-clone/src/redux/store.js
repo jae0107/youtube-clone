b554fc669b06776cbf0eba67d228803a3d82ccc4
jae0107 <ghjgjh0107@gmail.com>
2021-06-02 23:26:48 +0930

    add auth.reducer

--- youtube-clone/src/redux/store.js
+++ youtube-clone/src/redux/store.js
@@ -1,14 +1,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
+import { authReducer } from './reducers/auth.reducer'
 
-const initialState = {
-    name: "Sumit",
-    age: "21"
-}
+const rootReducer = combineReducers({
+    auth: authReducer
+});
 
-const reducer = (initialState) => initialState
-
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware(thunk)))
 
 export default store
