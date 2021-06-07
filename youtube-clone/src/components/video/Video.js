981cce8c90c5c58c9ad1f516e14818deefbb2f10
jae0107 <ghjgjh0107@gmail.com>
2021-06-07 18:20:26 +0930

    add skeleton and lazy load

--- youtube-clone/src/components/video/Video.js
+++ youtube-clone/src/components/video/Video.js
@@ -4,6 +4,7 @@ import { AiFillEye } from 'react-icons/ai'
 import request from '../../api'
 import moment from 'moment'
 import numeral from 'numeral'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const Video = ({video}) => {
     const {
@@ -57,8 +58,9 @@ const Video = ({video}) => {
     return (
         <div className="video">
             <div className="video__top">
-                <img src={medium.url} alt=''></img>
-                <span>{_duration}</span>
+                {/* <img src={medium.url} alt='' /> */}
+                <LazyLoadImage src={medium.url} effect='blur' />
+                <span className='video__top__duration'>{_duration}</span>
             </div>
             <div className="video__title">
                 {title}
@@ -70,7 +72,7 @@ const Video = ({video}) => {
                 <span>{moment(publishedAt).fromNow()}</span>
             </div>
             <div className="video__channel">
-                <img src={channelIcon?.url} alt=""></img>
+                <LazyLoadImage src={channelIcon?.url} effect='blur' />
                 <p>{channelTitle}</p>
             </div>
         </div>
--- youtube-clone/src/screens/homeScreen/HomeScreen.js
+++ youtube-clone/src/screens/homeScreen/HomeScreen.js
@@ -5,6 +5,7 @@ import Video from '../../components/video/Video'
 import { useDispatch, useSelector } from "react-redux"
 import { getPoplarVideos, getVideosByCategory } from '../../redux/actions/videos.action'
 import InfiniteScroll from 'react-infinite-scroll-component'
+import SkeletonVideo from '../../components/skeletons/SkeletonVideo'
 
 const HomeScreen = () => {
     const dispatch = useDispatch();
@@ -12,7 +13,7 @@ const HomeScreen = () => {
         dispatch(getPoplarVideos());
     }, [dispatch]);
 
-    const {videos, activeCategory} = useSelector(state => state.homeVideos);
+    const {videos, activeCategory, loading} = useSelector(state => state.homeVideos);
 
     const fetchData = () => {
         if(activeCategory === "All"){
@@ -30,11 +31,19 @@ const HomeScreen = () => {
             <Row>
                 <InfiniteScroll dataLength={videos.length} next={fetchData} hasMore={true} loader={<div className='spinner-border text-danger d-block mx-auto'></div>} className='row'>
                     {
-                        videos.map((video) => (
-                            <Col lg={3} md={4}>
-                                <Video  video={video} key={video.id}/>
-                            </Col>
-                        ))
+                        !loading ? 
+                            videos.map((video) => (
+                                <Col lg={3} md={4}>
+                                    <Video  video={video} key={video.id}/>
+                                </Col>
+                            ))
+                        : (
+                            [...Array(20)].map(() => 
+                                <Col lg={3} md={4}>
+                                    <SkeletonVideo />
+                                </Col>
+                            )
+                        )
                     }
                 </InfiniteScroll>
             </Row>
