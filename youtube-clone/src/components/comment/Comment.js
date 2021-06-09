1a86efafae28598acc6267b3f2d54348286dccac
jae0107 <ghjgjh0107@gmail.com>
2021-06-10 05:28:44 +0930

    add comments

--- youtube-clone/src/components/comment/Comment.js
+++ youtube-clone/src/components/comment/Comment.js
@@ -2,15 +2,17 @@ import React from 'react'
 import moment from 'moment'
 import './_comment.scss'
 
-const Comment = () => {
+const Comment = ({comment}) => {
+    const { authorDisplayName, authorProfileImageUrl, publishedAt, textDisplay } = comment;
+
     return (
         <div className='p-2 comment d-flex'>
-            <img src='https://www.pngkey.com/png/full/114-1149878_setting-user-avartar-in-specific-size-without-breaking.png' alt='avatar' className='mr-3 rounded-circle' />
+            <img src={authorProfileImageUrl} alt='avatar' className='mr-3 rounded-circle' />
             <div className='comment__body'>
                 <p className='mb-1 comment__header'>
-                    Sumit Day • {moment('2020-05-05').fromNow()}
+                    {authorDisplayName} • {moment(publishedAt).fromNow()}
                 </p>
-                <p className='mb-0'>Nice Video DUDE!!!</p>
+                <p className='mb-0'>{textDisplay}</p>
             </div>
         </div>
     )
--- youtube-clone/src/components/comments/Comments.js
+++ youtube-clone/src/components/comments/Comments.js
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addComment, getCommentsOfVideoById } from '../../redux/actions/comments.action'
 import Comment from '../comment/Comment'
 import './_comments.scss'
 
-const Comments = () => {
-    const handleComment = () => {
-        
+const Comments = ({videoId, totalComments}) => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCommentsOfVideoById(videoId));
+    }, [videoId, dispatch]);
+
+    const [text, setText] = useState('');
+
+    const comments = useSelector(state => state.commentList.comments);
+    const _comments = comments?.map(
+        comment => comment.snippet.topLevelComment.snippet
+    );
+
+    const handleComment = (e) => {
+        e.preventDefault();
+        if (text.length === 0) return
+
+        dispatch(addComment(videoId, text));
+        setText('');
     }
 
     return (
         <div className='comments'>
-            <p>1234 Comments</p>
+            <p>{totalComments} Comments</p>
             <div className='my-2 comments__form d-flex w-100'>
                 <img src='https://www.pngkey.com/png/full/114-1149878_setting-user-avartar-in-specific-size-without-breaking.png' alt='avatar' className='mr-3 rounded-circle' />
                 <form onSubmit={handleComment} className='d-flex flex-grow-1'>
-                    <input type='text' className='flex-grow-1' placeholder='Write a comment...'/>
+                    <input type='text' className='flex-grow-1' placeholder='Write a comment...' value={text} onChange={e => setText(e.target.value)}/>
                     <button className='p-2 border-0'>Comment</button>
                 </form>
             </div>
             <div className='comments__list'>
                 {
-                    [...Array(15)].map(() => (
-                        <Comment />
+                    _comments?.map((comment, i) => (
+                        <Comment comment={comment} key={i}/>
                     ))
                 }
             </div>
